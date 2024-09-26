36aec12538200b443e1ef9e4d742909f9c96b70d
Eduardo <el_alienx@hotmail.com>
2024-09-26 17:39:01 +0200

    fix radio fixtures

--- src/cosmos/components/InputRadio.fixture.tsx
+++ src/cosmos/components/InputRadio.fixture.tsx
@@ -8,20 +8,6 @@ const fakeDefaultState: [number, Function] = [-1, () => {}]; // -1 means no sele
 const fakeActiveState: [number, Function] = [0, () => {}]; // 0 = activates the first option
 
 export default {
-  Default: (
-    <InputRadio
-      id="color"
-      label="Choose your favorite color from this list:"
-      state={fakeDefaultState}
-      options={colorOptions}
-    />
-  ),
-  Active: (
-    <InputRadio
-      id="storage"
-      label="Choose an storage size that fits your needs:"
-      state={fakeActiveState}
-      options={storageOptions}
-    />
-  ),
+  Default: <InputRadio id="color" state={fakeDefaultState} options={colorOptions} />,
+  Active: <InputRadio id="storage" state={fakeActiveState} options={storageOptions} />,
 };
