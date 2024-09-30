ce0fd3a9dabbf528b004370bffe1d5b8ecac87fc
Eduardo Alvarez <el_alienx@hotmail.com>
2024-10-01 00:15:54 +0200

    adding component

---
+++ src/cosmos/components/ToastNotification.fixture.tsx
@@ -0,0 +1,24 @@
+// Project files
+import ToastNotification from "components/toast-notification/ToastNotification";
+
+// Node modules
+import { Toaster } from "react-hot-toast";
+import toast from "react-hot-toast";
+
+function Decorator() {
+  // Methods
+  function openToast() {
+    toast(<ToastNotification title={"Added to cart"} icon={"bag-shopping"} color={"red"} />);
+  }
+
+  return (
+    <div>
+      <button onClick={openToast}>Open toast</button>
+      <Toaster position="bottom-right" />
+    </div>
+  );
+}
+
+export default {
+  Default: <Decorator />,
+};
