8be6716c0e51f1e03b5d4b980e09a1b4fb943827
Eduardo Alvarez <el_alienx@hotmail.com>
2024-10-01 00:36:52 +0200

    added toaster

--- src/cosmos/components/ToastNotification.fixture.tsx
+++ src/cosmos/components/ToastNotification.fixture.tsx
@@ -4,21 +4,38 @@ import ToastNotification from "components/toast-notification/ToastNotification";
 // Node modules
 import { Toaster } from "react-hot-toast";
 import toast from "react-hot-toast";
+import Color from "types/Color";
+import IconPrefix from "types/IconPrefix";
 
-function Decorator() {
+// Decorator
+interface Props {
+  /** The text to display */
+  title: string;
+
+  /** The icon category of FontAwesome library. This can be the "fas" solid icons or the "fab" brand icons. */
+  icon_prefix?: IconPrefix;
+
+  /** The icon name from the FontAwesome library. */
+  icon: string;
+
+  color: Color;
+}
+
+function Decorator({ title, icon, color }: Props) {
   // Methods
   function openToast() {
-    toast(<ToastNotification title={"Added to cart"} icon={"bag-shopping"} color={"green"} />);
+    toast(<ToastNotification title={title} icon={icon} color={color} />);
   }
 
   return (
     <div>
-      <button onClick={openToast}>Open toast</button>
+      <button onClick={openToast}>Open {title}</button>
       <Toaster position="bottom-right" toastOptions={{ className: "toaster" }} />
     </div>
   );
 }
 
 export default {
-  Default: <Decorator />,
+  Default: <Decorator title={"Added to cart"} icon={"bag-shopping"} color={"green"} />,
+  Red: <Decorator title={"Deleted"} icon={"tash-can"} color={"red"} />,
 };
