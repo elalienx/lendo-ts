ef9a5e42f7e794fef7789467f9c3849fc5baa949
Eduardo <el_alienx@hotmail.com>
2024-10-03 15:09:23 +0200

    small update

--- src/state/actions/addItem.ts
+++ src/state/actions/addItem.ts
@@ -3,8 +3,6 @@ import type CartItem from "types/CartItem";
 
 export default function addItem(state: CartItem[], payload: CartItem) {
   const newItem = payload;
-
-  // Safeguards: Check if the item already exists in the cart with the same options
   const itemIndex = state.findIndex(
     (item) =>
       item.product_id === newItem.product_id &&
@@ -12,19 +10,13 @@ export default function addItem(state: CartItem[], payload: CartItem) {
       item.variantIndex === newItem.variantIndex
   );
 
-  // If the item doesn't exist, add it to the cart
-  if (itemIndex === -1) {
-    return [...state, newItem];
-  }
+  // Safeguard
+  if (itemIndex === -1) return [...state, newItem];
 
-  // If the item exists, update the quantity
   const updatedState = [...state];
-
-  // Create a deep copy of the item to avoid mutating the original state
   const updatedItem = { ...updatedState[itemIndex] };
-  updatedItem.selectedQuantity += newItem.selectedQuantity;
 
-  // Replace the old item with the updated item
+  updatedItem.selectedQuantity += newItem.selectedQuantity;
   updatedState[itemIndex] = updatedItem;
 
   return updatedState;
