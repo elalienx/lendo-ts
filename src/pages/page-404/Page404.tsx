0f5b56ecec54cb39941a2a2f8b1954ebae811849
Eduardo Alvarez <el_alienx@hotmail.com>
2024-10-05 10:31:33 +0200

    added and passed the first Playwright tests

--- src/pages/page-404/Page404.tsx
+++ src/pages/page-404/Page404.tsx
@@ -1,9 +1,17 @@
+// Node modules
+import { useEffect } from "react";
+
 // Project files
 import EmptyState from "components/empty-state/EmptyState";
 import EmptyStateText from "./empty-state-text.json";
 import "./page-404.css";
 
 export default function Page404() {
+  // Methods
+  useEffect(() => {
+    document.title = "Page not found | Lendo";
+  }, []);
+
   return (
     <div id="page-404" className="page">
       <EmptyState item={EmptyStateText} />
