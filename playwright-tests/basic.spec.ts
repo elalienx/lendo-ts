81678215b3e2c9aecbc2205eb483c06a62caa513
Eduardo Alvarez <el_alienx@hotmail.com>
2024-10-05 11:00:22 +0200

    switched to localhost for faster E2E tests

--- playwright-tests/basic.spec.ts
+++ playwright-tests/basic.spec.ts
@@ -3,11 +3,11 @@ import { test, expect } from "@playwright/test";
 
 test("Has title", async ({ page }) => {
   // Arrange
-  const url = "http://lendo-ts.web.app";
+  const localhost = "/";
   const result = /Lendo/i;
 
   // Act
-  await page.goto(url);
+  await page.goto(localhost);
 
   // Assert
   // Expect a the <head><title> "to contain" a substring.
@@ -16,11 +16,11 @@ test("Has title", async ({ page }) => {
 
 test("Get cart link", async ({ page }) => {
   // Arrange
-  const url = "http://lendo-ts.web.app";
+  const localhost = "/";
   const result = /your cart is empty/i;
 
   // Act
-  await page.goto(url);
+  await page.goto(localhost);
   await page.getByRole("link", { name: "Cart", exact: true }).click();
 
   // Assert
